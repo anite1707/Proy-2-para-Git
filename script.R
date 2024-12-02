7764ba2367ef9497ebf8b37d5ab94516ad025fae
Anaís A-G. <anais.aluicio@gmail.com>
2024-12-02 11:32:45 -0300

    ordenamiento

--- script.R
+++ script.R
@@ -1,1 +1,7 @@
 library(tidyverse)
+git config --global user.email "anais.aluicio@gmail.com"
+git config --global user.name "Anaís A-G."
+ggplot()
+usethis::use_github()
+usethis::gh_token_help()
+gitcreds::gitcreds_set()
